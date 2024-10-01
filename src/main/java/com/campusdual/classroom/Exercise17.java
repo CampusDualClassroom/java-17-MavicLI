443618079828c37c8ae27b7feb283a4a745e6dc3
Mavic Lamela Irureta <[mavic_81@hotmail.com]>
2024-10-01 09:51:13 +0200

    Ejercicio 17 Resuelto

--- src/main/java/com/campusdual/classroom/Exercise17.java
+++ src/main/java/com/campusdual/classroom/Exercise17.java
@@ -1,15 +1,34 @@
 package com.campusdual.classroom;
 
 public class Exercise17 {
-	public static int[] createEmptyIntArray(int positions) {}
+	//nuevo array de enteros
+	public static int[] createEmptyIntArray(int positions) {
+		return new int[positions];
+	}
+	//nuevo array inicializado
+	public static String[] createInitializedStringArray() {
+		return new String[]{"1","2","3"};
+	}
 
-	public static String[] createInitializedStringArray() {}
+	public static void populateArray(int[] array, int index, int value) {
+		if (index >=0 && index < array.length) {
+			array[index] = value;
+		}else {
+			System.out.println("Indice fuera de rango");
+		}
+	}
 
-	public static void populateArray(int[] array, int index, int value) {}
+	public static int arrayLength(int[] array) {
+		return array.length;
+	}
 
-	public static int arrayLength(int[] array) {}
-
-	public static void showValue(int[] array, int index) {}
+	public static void showValue(int[] array, int index) {
+		if (index >= 0 && index < array.length){
+			System.out.println("Valor de la posicion " + index + ":" + array[index]);
+		}else {
+			System.out.println("Indice fuera de rango");
+		}
+	}
 
 	public static void main(String[] args) {
 		int[] intArray = createEmptyIntArray(3);
